114ac6d8ca7e437175cbb78657f9632eb765b639
Jean-Baptiste WITTNER <jeanbaptiste.wittner@outlook.com>
2020-05-25 00:31:33 +0200

    Update

--- server/src/main/java/fr/jbwittner/myguild/server/MyGuildApplication.java
+++ server/src/main/java/fr/jbwittner/myguild/server/MyGuildApplication.java
@@ -1,4 +1,4 @@
-package fr.jbwittner.myguild;
+package fr.jbwittner.myguild.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- server/src/test/java/fr/jbwittner/myguild/server/MyGuildApplicationTests.java
+++ server/src/test/java/fr/jbwittner/myguild/server/MyGuildApplicationTests.java
@@ -1,4 +1,4 @@
-package fr.jbwittner.myguild;
+package fr.jbwittner.myguild.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
