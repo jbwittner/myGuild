3bd8afc3218e4c25b723a9347e48b244696a065f
Jean-Baptiste WITTNER <jeanbaptiste.wittner@outlook.com>
2020-05-25 11:15:37 +0200

    Update travis file

--- server/src/main/java/fr/jbwittner/myguild/server/MyGuildApplication.java
+++ server/src/main/java/fr/jbwittner/myguild/server/MyGuildApplication.java
@@ -3,10 +3,17 @@ package fr.jbwittner.myguild.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Launcher of the application
+ * @author Jean-Baptiste WITTNER
+ */
 @SpringBootApplication
 public class MyGuildApplication {
 
-	public static void main(String[] args) {
+    /**
+     * Main of the application
+     */
+    public static void main(final String[] args) {
 		SpringApplication.run(MyGuildApplication.class, args);
 	}
 
--- server/src/test/java/fr/jbwittner/myguild/server/MyGuildApplicationTests.java
+++ server/src/test/java/fr/jbwittner/myguild/server/MyGuildApplicationTests.java
@@ -3,9 +3,16 @@ package fr.jbwittner.myguild.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Launcher of the application test
+ * @author Jean-Baptiste WITTNER
+ */
 @SpringBootTest
 class MyGuildApplicationTests {
 
+    /**
+     * Test
+     */
 	@Test
 	void contextLoads() {
 	}
