c52c03fd9bec87446a1849f53f490964ba5d58dd
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2020-06-08 23:26:41 +0200

    ref. #3 : Modification of repositories

--- server/src/main/java/fr/jbwittner/myguild/server/repository/UserAccountRepository.java
+++ server/src/main/java/fr/jbwittner/myguild/server/repository/UserAccountRepository.java
@@ -1,13 +1,11 @@
 package fr.jbwittner.myguild.server.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import fr.jbwittner.myguild.server.model.UserAccount;
 
 /**
  * Repository for UserAccount
  */
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+public interface UserAccountRepository extends AbstractRepository<UserAccount, Long> {
 
     /**
      * Method to find UserAccount by email
@@ -15,12 +13,5 @@ public interface UserAccountRepository extends JpaRepository<UserAccount, Long> 
      * @return UserAccount which the email match
      */
     UserAccount findByEmail(String email);
-
-    /**
-     * Method to delete UserAccount
-     * @param userAccount UserAccount to delete
-     */
-    @Override
-    void delete(UserAccount userAccount);
 
 }
