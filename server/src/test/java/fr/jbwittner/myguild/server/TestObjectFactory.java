a7959be1c4a0058965e19091c5aec269fd2dbf16
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2020-06-08 23:26:55 +0200

    ref. #3 : Adding tests

---
+++ server/src/test/java/fr/jbwittner/myguild/server/TestObjectFactory.java
@@ -0,0 +1,213 @@
+package fr.jbwittner.myguild.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import fr.jbwittner.myguild.server.model.Roles;
+import fr.jbwittner.myguild.server.model.UserAccount;
+
+/**
+ * Factory to help during tests
+ */
+public class TestObjectFactory {
+
+    public static final int NUMBER_MAX = 100;
+    public static final int LENGTH_EMAIL = 20;
+
+    private List<String> listRandomString = new ArrayList<>();
+    private List<String> listRandomEmail = new ArrayList<>();
+    private List<Integer> listRandomNumber = new ArrayList<>();
+
+    /**
+     * Method to call when we need to reset the list of random strings
+     */
+    public void resetListString(){
+        this.listRandomString = new ArrayList<>();
+    }
+
+    /**
+     * Method to call when we need to reset the list of random integer
+     */
+    public void resetListInteger(){
+        this.listRandomNumber = new ArrayList<>();
+    }
+
+    /**
+     * Method to call when we need to reset the list of random email
+     */
+    public void resetListEmail(){
+        this.listRandomEmail = new ArrayList<>();
+    }
+
+    /**
+     * Method to call when we need to reset all lists
+     */
+    public void resetAllList(){
+        this.resetListInteger();
+        this.resetListString();
+        this.resetListEmail();
+    }
+
+    /**
+     * Generate a random ascii string.
+     * When a new string is generated.
+     * It is stored on a list used to check that the same chain is not obtained after reuse
+     * @param length Length of the string
+     * @return String generated
+     */
+    public String getUniqueRandomAsciiString(final int length){
+        boolean isNotUnique = true;
+        String randomString = "";
+
+        while (isNotUnique){
+            randomString = RandomStringUtils.randomAscii(length);
+            isNotUnique = listRandomString.contains(randomString);
+        }
+
+        listRandomString.add(randomString);
+
+        return randomString;
+    }
+
+    /**
+     * Generate a random email.
+     * When a new email is generated.
+     * It is stored on a list used to check that the same email is not obtained after reuse
+     * @return email generated
+     */
+    public String getUniqueRandomEmail(){
+        boolean isNotUnique = true;
+        String email = "";
+
+        while (isNotUnique){
+            email = RandomStringUtils.randomAscii(LENGTH_EMAIL);
+            email = email + "@" + RandomStringUtils.randomAscii(LENGTH_EMAIL) + ".com";
+            isNotUnique = listRandomEmail.contains(email);
+        }
+
+        listRandomEmail.add(email);
+
+        return email;
+
+    }
+
+    /**
+     * Generate a random integer.
+     * When a new integer is generated.
+     * It is stored on a list used to check that the integer chain is not obtained after reuse
+     * @param min Min value of random integer
+     * @param max Max value of random integer
+     * @return integer generated
+     */
+    public Integer getUniqueRandomInteger(final Integer min, final Integer max){
+        boolean isNotUnique = true;
+        Integer randomNumber = 0;
+
+        while (isNotUnique){
+            randomNumber = this.getRandomInteger(min, max);
+            isNotUnique = listRandomNumber.contains(randomNumber);
+        }
+
+        listRandomNumber.add(randomNumber);
+
+        return randomNumber;
+    }
+
+    /**
+     * Generate a random integer.
+     * When a new integer is generated.
+     * It is stored on a list used to check that the integer chain is not obtained after reuse
+     * @param max Max value of random integer
+     * @return integer generated
+     */
+    public Integer getUniqueRandomInteger(final Integer max){
+        boolean isNotUnique = true;
+        Integer randomNumber = 0;
+
+        while (isNotUnique){
+            randomNumber = this.getRandomInteger(max);
+            isNotUnique = listRandomNumber.contains(randomNumber);
+        }
+
+        listRandomNumber.add(randomNumber);
+
+        return randomNumber;
+    }
+
+    /**
+     * Generate a random integer.
+     * When a new integer is generated.
+     * It is stored on a list used to check that the integer chain is not obtained after reuse
+     * @return integer generated
+     */
+    public Integer getUniqueRandomInteger(){
+        return this.getUniqueRandomInteger(NUMBER_MAX);
+    }
+
+    /**
+     * Generate a random integer.
+     * @param max Max value of random integer
+     * @return integer generated
+     */
+    public Integer getRandomInteger(final Integer max){
+        return (int) Math.random() * max;
+    }
+
+    /**
+     * Generate a random integer.
+     * @return integer generated
+     */
+    public Integer getRandomInteger(){
+        return this.getRandomInteger(NUMBER_MAX);
+    }
+
+    /**
+     * Generate a random integer.
+     * @param min Min value of random integer
+     * @param max Max value of random integer
+     * @return integer generated
+     */
+    public Integer getRandomInteger(final Integer min, final Integer max){
+        return (int) (min + (Math.random() * (max - min)));
+    }
+
+    /**
+     * Generate a new user account.
+     * @param email email of the account
+     * @param nickName nickName of the account
+     * @param password password of the account
+     * @param enabled enable the account
+     * @param roles roles of the account
+     * @return user account generated
+     */
+    public UserAccount createUserAccount(final String email, final String nickName,
+                                        final String password, final Boolean enabled,
+                                        final List<Roles> roles){
+        
+        final UserAccount userAccount = new UserAccount(email, nickName, password, enabled, roles);
+
+        return userAccount;
+    }
+
+    /**
+     * Generate a new user account with a random values.
+     * @return user account generated
+     */
+    public UserAccount createUserAccount(){
+
+        final String email = this.getUniqueRandomEmail();
+        final String nickName = this.getUniqueRandomAsciiString(LENGTH_EMAIL);
+        final String password = this.getUniqueRandomAsciiString(LENGTH_EMAIL);
+        final Boolean enabled = false;
+        
+        final List<Roles> roles = new ArrayList<>();
+        roles.add(Roles.ROLES_USER);
+        
+        final UserAccount userAccount = new UserAccount(email, nickName, password, enabled, roles);
+
+        return userAccount;
+    }
+
+}
---
+++ server/src/test/java/fr/jbwittner/myguild/server/it/AbstractMotherTest.java
@@ -0,0 +1,45 @@
+package fr.jbwittner.myguild.server.it;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.jbwittner.myguild.server.TestObjectFactory;
+
+/**
+ * Mother class for integrations tests
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Transactional
+public abstract class AbstractMotherTest {
+
+    protected TestObjectFactory factory = new TestObjectFactory();
+
+    /**
+     * Method launch before each test
+     */
+    @BeforeEach
+    public void beforeEach(){
+        this.initDataBeforeEach();
+    }
+
+    /**
+     * Method launch after each test
+     */
+    @AfterEach
+    public void afterEach(){
+        this.factory.resetListString();
+    }
+
+    /**
+     * Method used to prepare the data of tests
+     */
+    abstract protected void initDataBeforeEach();
+
+}
