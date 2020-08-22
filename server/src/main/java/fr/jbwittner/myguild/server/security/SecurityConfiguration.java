e956053ff351e9b6e63236146701a791d6cbe9ca
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2020-08-22 16:03:19 +0200

    Adding security configuration to use oauth2 to authentication

--- server/src/main/java/fr/jbwittner/myguild/server/security/SecurityConfiguration.java
+++ server/src/main/java/fr/jbwittner/myguild/server/security/SecurityConfiguration.java
@@ -1,10 +1,8 @@
 package fr.jbwittner.myguild.server.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Class configuration used to configure spring security
@@ -12,14 +10,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    /**
-     * Method used to set the password encoder of the application
-     * @return PasswordEncoder used to encrypt passwords
-     */
-    @Bean
-    public PasswordEncoder encoder() {
-        final PasswordEncoder encoder = new BCryptPasswordEncoder();
-        return encoder;
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http
+            .antMatcher("/**").authorizeRequests()
+            .antMatchers("/").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .oauth2Login();
     }
-    
+        
 }
