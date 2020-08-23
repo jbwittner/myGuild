be5af3867fd6aa3ebbed77c1190f73b086590924
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2020-08-23 23:49:06 +0200

    Mock in progress

--- server/src/test/java/fr/jbwittner/myguild/server/it/service/oauth2/OAuth2FlowHandlerImplTest.java
+++ server/src/test/java/fr/jbwittner/myguild/server/it/service/oauth2/OAuth2FlowHandlerImplTest.java
@@ -30,28 +30,17 @@ import static org.mockito.Mockito.*;
 public class OAuth2FlowHandlerImplTest {
 
     @Mock
-    private AppConfig appConfig;
-    @Mock
-    private EnvConfig envConfig;
-    @Mock
     private ObjectMapper objectMapper;
 
     @InjectMocks
     private BlizzardOAuth2FlowHandler blizzardOAuth2FlowHandler;
 
+    
     @Test
     public void getTokenFreshGoldenPath() throws IOException, NoSuchFieldException {
         final String token = "exampleToken";
-        final HttpURLConnection mockUrlConnection = mock(HttpURLConnection.class);
         final OutputStream outputStream = mock(OutputStream.class);
         final TokenResponse mockTokenResponse = mock(TokenResponse.class);
-
-        final URLStreamHandler urlStreamHandler = new URLStreamHandler() {
-            @Override
-            protected URLConnection openConnection(URL u) throws IOException {
-                return mockUrlConnection;
-            }
-        };
 
         String clientId = "someClientId";
         String clientSecret = "someClientSecret";
@@ -62,35 +51,14 @@ public class OAuth2FlowHandlerImplTest {
             String.format("{'access_token':'%s', 'expires_in':'1'}", token).getBytes("UTF-8")
         );
 
-        doReturn(clientId).when(envConfig).getClientId();
-        doReturn(clientSecret).when(envConfig).getClientSecret();
-        doReturn(encodeFormat).when(appConfig).getEncoding();
-        doReturn(new URL("http://www.google.com/")).when(appConfig).getTokenUrl();
+        FieldSetter.setField(blizzardOAuth2FlowHandler, blizzardOAuth2FlowHandler.getClass().getDeclaredField("tokenExpiry"), null);
+        FieldSetter.setField(blizzardOAuth2FlowHandler, blizzardOAuth2FlowHandler.getClass().getDeclaredField("token"), null);
+        FieldSetter.setField(blizzardOAuth2FlowHandler, blizzardOAuth2FlowHandler.getClass().getDeclaredField("tockenUrl"), "https://google.com/");
+        
         doReturn(mockTokenResponse).when(objectMapper).readValue(anyString(), eq(TokenResponse.class));
-
-        FieldSetter.setField(blizzardOAuth2FlowHandler, blizzardOAuth2FlowHandler.getClass().getDeclaredField("urlStreamHandler"), urlStreamHandler);
-
-        doReturn(byteArrayInputStream).when(mockUrlConnection).getInputStream();
-        doReturn(outputStream).when(mockUrlConnection).getOutputStream();
-        doReturn(responseCode).when(mockUrlConnection).getResponseCode();
-
-        doReturn(token).when(mockTokenResponse).getAccess_token();
-
-        Assert.assertEquals(token, blizzardOAuth2FlowHandler.getToken());
-        verify(mockUrlConnection, times(1)).setRequestMethod("POST");
-        verify(mockUrlConnection, times(1))
-            .setRequestProperty(
-                "Authorization",
-                String.format("Basic %s",
-                    Base64.getEncoder().encodeToString(
-                        String.format("%s:%s", clientId, clientSecret).getBytes(encodeFormat)
-                    )
-                )
-            );
-        verify(mockUrlConnection, times(1)).setDoOutput(true);
-        verify(outputStream, times(1)).write("grant_type=client_credentials".getBytes(encodeFormat));
-        verify(mockUrlConnection, times(1)).getResponseCode();
+        Assert.assertEquals("token", blizzardOAuth2FlowHandler.getToken());
     }
+    
 
     @Test
     public void getTokenCachedGoldenPath() throws NoSuchFieldException, IOException {
@@ -107,7 +75,6 @@ public class OAuth2FlowHandlerImplTest {
         FieldSetter.setField(blizzardOAuth2FlowHandler, blizzardOAuth2FlowHandler.getClass().getDeclaredField("tokenExpiry"), Instant.now().plus(5, ChronoUnit.MINUTES));
         FieldSetter.setField(blizzardOAuth2FlowHandler, blizzardOAuth2FlowHandler.getClass().getDeclaredField("token"), "SomeSampleToken");
         Assert.assertFalse(blizzardOAuth2FlowHandler.isTokenInvalid());
-
     }
 
     @Test
@@ -115,7 +82,6 @@ public class OAuth2FlowHandlerImplTest {
         FieldSetter.setField(blizzardOAuth2FlowHandler, blizzardOAuth2FlowHandler.getClass().getDeclaredField("tokenExpiry"), null);
         FieldSetter.setField(blizzardOAuth2FlowHandler, blizzardOAuth2FlowHandler.getClass().getDeclaredField("token"), null);
         Assert.assertTrue(blizzardOAuth2FlowHandler.isTokenInvalid());
-
     }
 
     @Test
