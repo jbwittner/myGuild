6534bcfca51e3a52b03b8fd9f3f2ee67a22c4fe6
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2020-10-12 17:00:50 +0200

    Add data fetch from api

--- server/src/main/java/fr/opendoha/myguild/server/service/IBlizzardService.java
+++ server/src/main/java/fr/opendoha/myguild/server/service/IBlizzardService.java
@@ -1,8 +1,12 @@
 package fr.opendoha.myguild.server.service;
 
+import fr.opendoha.myguild.server.parameters.BlizzardAccountParameter;
+
 /**
  * Interface for the blizzard service
  */
 public interface IBlizzardService {
+
+    void fetchAccountCharacter(final BlizzardAccountParameter blizzardAccountParameter);
 
 }
