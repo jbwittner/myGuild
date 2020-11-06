2ebc283c89fdef2d020a502c93159681718d023b
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2020-11-07 00:20:17 +0100

    Wip front

--- frontend/src/services/home/page/HomePage.tsx
+++ frontend/src/services/home/page/HomePage.tsx
@@ -5,21 +5,28 @@ import { SessionStorage } from '../../storage/SessionStorage';
 export default function HomePage() {
 
     const staticData = SessionStorage.getItem<StaticDataDTO>(SessionStorage.STATIC_DATA);
-    const accountCharacters = SessionStorage.getItem<CharacterSummaryDTO[]>(SessionStorage.ACCOUNT_CHARACTERS);
+    const accountCharacters = SessionStorage.getItem<any>(SessionStorage.ACCOUNT_CHARACTERS);
 
     console.log(staticData)
-    console.log(accountCharacters as CharacterSummaryDTO[])
+    console.log(accountCharacters.characterSummaryDTOs)
     
-    const test = accountCharacters as CharacterSummaryDTO[]
+    const test = accountCharacters.characterSummaryDTOs as CharacterSummaryDTO[]
     
     if(accountCharacters !== undefined){
         console.log(test[0])
     }
+
+    const tata = test.map((value) => 
+        <div key={value.name}>
+            {value.name}
+        </div>
+        
+    )
     
 
     return(
         <div>
-
+            {tata}
         </div>
 
     )
