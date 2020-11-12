7077900061d2003e1e16e2d29e0a90f4b2c1d0be
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2020-11-12 09:46:18 +0100

    Modification of homepage

--- frontend/src/services/home/page/HomePage.tsx
+++ frontend/src/services/home/page/HomePage.tsx
@@ -1,13 +1,9 @@
-import { Button } from '@material-ui/core';
 import React from 'react'
-import { useHistory } from 'react-router-dom';
 
 export default function HomePage() {
 
-    const history = useHistory();
-
     return(
-    <Button variant="contained" onClick={() => history.push("/charactersPage")}>{'kdlsmjgfqslkfj'}</Button>
+    <React.Fragment></React.Fragment>
     )
 
 }
